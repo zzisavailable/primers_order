6b21ff79292e030ba7ae80fe2b8105f58a347bd9
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-05-15 15:21:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ name_list = df.iloc[:, 0].tolist()
 selected_name = st.sidebar.selectbox('Select a primer name:', options=name_list)
 
 selected_row = df.loc[df['Name'] == selected_name]
+
+st.write(selected_row)
 to_order_df.loc[len(to_order_df)] = selected_row
 
 st.write(to_order_df)
