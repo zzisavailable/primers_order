808b527f4bbb5255f6bf82edf9cc2b5d8bdfe064
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-05-15 15:12:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 import pandas as pd
+from st_aggrid import AgGrid
 
 
 df = pd.read_csv('primers.csv')
-st.dataframe(df)
+AgGrid(df)
+
+to_order_df = pd.DataFrame(columns=df.columns)
+
+name_list = df.iloc[:, 0].tolist()
+selected_name = st.sidebar.selectbox(name_list)
+
+selected_row = df.loc[df['Name'] == selected_name]
+to_order_df.append(selected_row)
+
+st.write(to_order_df)
+
