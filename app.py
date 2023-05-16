bd4f8b1d1a33098fa137f901504d0fefbb8a6d2f
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-05-16 12:14:21 -0500

    update

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import pandas as pd
+from st_aggrid import AgGrid, GridUpdateMode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
 
-# create a dataframe
-df = pd.DataFrame({
-    "Name": ["John", "Jane", "Mike", "Sarah"],
-    "Age": [25, 30, 35, 40],
-    "City": ["New York", "Los Angeles", "Chicago", "Houston"]
-})
 
-# add checkboxes and buttons to the dataframe
-df["Select"] = [st.checkbox("", False, key=i) for i in range(len(df))]
-# df["Button"] = [st.button("Edit", key=i) for i in range(len(df))]
 
-# display the dataframe with checkboxes and buttons
-st.table(df)
+df = pd.read_csv('primers.csv')
+gd = GridOptionsBuilder.from_dataframe(df)
+gd.configure_selection(selection_mode='multiple', use_checkbox=True)
+gridoptions = gd.build()
+
+grid_table = AgGrid(df, height=250, gridOptions=gridoptions,
+                    update_mode=GridUpdateMode.SELECTION_CHANGED)
+
+st.write('## Selected')
+selected_row = grid_table["selected_rows"]
+st.dataframe(selected_row)
