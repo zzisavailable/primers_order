5e744d7c143c7aeebfa8365b7c1afc9f11f41a51
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-05-16 11:57:32 -0500

    update

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import streamlit as st
 import pandas as pd
-from st_aggrid import AgGrid
 
+# create a dataframe
+df = pd.DataFrame({
+    "Name": ["John", "Jane", "Mike", "Sarah"],
+    "Age": [25, 30, 35, 40],
+    "City": ["New York", "Los Angeles", "Chicago", "Houston"]
+})
 
-df = pd.read_csv('primers.csv')
-AgGrid(df)
+# add checkboxes and buttons to the dataframe
+df["Select"] = [st.checkbox("", False, key=i) for i in range(len(df))]
+df["Button"] = [st.button("Edit", key=i) for i in range(len(df))]
 
-to_order_df = pd.DataFrame(columns=df.columns)
-
-name_list = df.iloc[:, 0].tolist()
-selected_name = st.sidebar.selectbox('Select a primer name:', options=name_list)
-
-selected_row = df.loc[df['Name'] == selected_name]
-
-st.write(selected_row)
-to_order_df.loc[len(to_order_df)] = {'Name': 'test'}
-
-st.write(to_order_df)
-
+# display the dataframe with checkboxes and buttons
+st.dataframe(df)
